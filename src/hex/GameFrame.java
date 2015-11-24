85c756bb6ce5a1ad350e07bf6bc926dce75aace9
atrain99 <atrain99@mchsi.com>
2015-11-23 21:19:46 -0600

    I was gonna work, I promise

---
+++ src/hex/GameFrame.java
@@ -0,0 +1,21 @@
+package hex;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * 
+ */
+public class GameFrame extends JPanel{
+    
+    public GameFrame(){
+        
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        
+    }
+    
+}
